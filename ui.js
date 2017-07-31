72c4bcc9cb34b0b490e8a52551e1c7db8483c5e9
trcka <michael.trcka@ttu.edu>
2017-07-30 21:08:17 -0600

    more stuff

--- ui.js
+++ ui.js
@@ -1,3 +1,18 @@
 var WeatherAdmin = require("./clozeCard");
 var WeatherAdmin = require("./basicCard");
 var inquirer = require("inquirer");
+
+inquirer.prompt([
+  name:"action",
+  type:"list",
+  message:"What do you want to do?",
+  choices:[
+    "Make Basic Type Flash Cards",
+    "Make Cloze Type Flash Cards",
+    "Review Basic Type Flash Cards",
+    "Review Cloze Type Flash Cards"
+  ]
+])
+.then(function(ans){
+  
+})
